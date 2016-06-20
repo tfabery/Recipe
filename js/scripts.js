a50df42b47e43501df40535a3d053b1d358f60c0
Alvin Ng and Tony Fabery <tony@>
2016-06-20 16:46:53 -0700

    fix carousel to work with bootstrap 4.0 and shorten carousel slide interval

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,33 @@
 //==============================BackEnd==================================
 
 //==============================FrontEnd=================================
-var $item = $('.carousel .item');
-var $wHeight = $(window).height();
-
-$item.height($wHeight);
-$item.addClass('full-screen');
-
-$('.carousel img').each(function() {
-  var $src = $(this).attr('src');
-  var $color = $(this).attr('data-color');
-  $(this).parent().css({
-    'background-image' : 'url(' + $src + ')',
-    'background-color' : $color
-  });
-  $(this).remove();
-});
-
-$(window).on('resize', function (){
-  $wHeight = $(window).height();
-  $item.height($wHeight);
-});
 
 $(function() {
+  var $item = $('.carousel .carousel-item');
+  var $wHeight = $(window).height();
+
+  $item.height($wHeight);
+  $item.addClass('full-screen');
+
+  $('.carousel img').each(function() {
+    var $src = $(this).attr('src');
+    var $color = $(this).attr('data-color');
+    $(this).parent().css({
+      'background-image' : 'url(' + $src + ')',
+      'background-color' : $color
+    });
+    $(this).remove();
+  });
+
+  $(window).on('resize', function (){
+    $wHeight = $(window).height();
+    $item.height($wHeight);
+  });
+
+  $('.carousel').carousel({
+    interval: 300 * 10
+  });
+  
   $('form#myForm').submit(function(event) {
     event.preventDefault();
   });
