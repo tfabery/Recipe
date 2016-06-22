fbfbf2fc2d4c1231f6e27613a91b835d3c20fb8a
Alvin Ng and Tony Fabery <alvin@>
2016-06-22 15:09:11 -0700

    add popover price for all dem ingredients

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,36 @@
 //==============================BackEnd==================================
+var apiKey = '6f1739ee3b';
 
+function callOtherDomain(name){
+  var url = 'http://www.supermarketapi.com/api.asmx/COMMERCIAL_SearchByProductName?APIKEY=6f1739ee3b&ItemName=' + name;
+  var request = new XMLHttpRequest();
+  if(request) {
+    request.open('GET', url, false);
+    request.send();
+    var xml = request.responseXML;
+    var prices = xml.getElementsByTagName("Pricing");
+    var priceArray = [];
+    for (var i = 0; i < prices.length; i ++) {
+      var price = prices[i].childNodes[0].nodeValue;
+      priceArray.push(price);
+      // alert(price);
+    }
+    priceArray.sort(function(a, b) {return a-b});
+    return parseFloat(priceArray[0]).toFixed(2);
+  }
+}
 
-
-
-
-
+function addPopover(span){
+  var ingredient = $(span).text();
+  var itemName = ingredient[0].toUpperCase() + ingredient.substring(1);
+  // $(span).tooltip();
+  $(span).attr('data-toggle', 'popover');
+  $(span).attr('data-placement', 'top');
+  $(span).attr('data-content', "Price: " + '$' + callOtherDomain(itemName));
+  $(span).attr('title', itemName);
+  $(span).popover();
+}
 //==============================FrontEnd=================================
-
 $(function() {
   var $item = $('.carousel .carousel-item');
   var $wHeight = $(window).height();
@@ -48,6 +72,6 @@ $(function() {
     $('#landingPage').hide();
     $('.col-md-4:not(.' + cuisine + '.' + temperature + ')').hide();
     $('.ad').show();
+    addPopover('span.ingredient');
   });
-
 });
